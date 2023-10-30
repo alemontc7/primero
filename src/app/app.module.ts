a96aa4dc0e2ef1d2cad1c94b14318a139ebfc9a7
Alejandro Montero Coca <alejandro.montero@ucb.edu.bo>
2023-10-30 16:11:35 -0400

    tiene nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { ContactoComponent } from './paginas/contacto/contacto.component';
+import { ProductosComponent } from './paginas/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ContactoComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
